f24d4ec509df681a2d7cdecd12ed225d68fc54cf
Kevin Jullien <kevin.jullien@student.vinci.be>
2021-03-02 22:22:59 +0100

    refactor - mise en commentaire d'un attribut non utilisé pour Jenkins PMD

--- src/main/java/be/vinci/pae/domain/UtilisateurUCCImpl.java
+++ src/main/java/be/vinci/pae/domain/UtilisateurUCCImpl.java
@@ -1,13 +1,10 @@
 package be.vinci.pae.domain;
 
-import be.vinci.pae.services.UtilisateurDAO;
-
-import jakarta.inject.Inject;
-
 public class UtilisateurUCCImpl implements UtilisateurUCC {
 
-  @Inject
-  private UtilisateurDAO utilisateurDAO;
+  /*
+   * @Inject private UtilisateurDAO utilisateurDAO;
+   */
 
 
   @Override
