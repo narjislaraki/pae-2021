6a9c88586e50baf813e61506caf03417894b9e02
Kevin Jullien <kevin.jullien@student.vinci.be>
2021-03-04 11:48:36 +0100

    refactor - adapté à l'archi du cours

--- src/main/java/be/vinci/pae/services/UtilisateurDAOImpl.java
+++ src/main/java/be/vinci/pae/services/UtilisateurDAOImpl.java
@@ -1,37 +1,14 @@
 package be.vinci.pae.services;
 
 import be.vinci.pae.domain.Utilisateur;
-import be.vinci.pae.utils.Config;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class UtilisateurDAOImpl implements UtilisateurDAO {
-  private Connection conn = null;
-
+  
   public UtilisateurDAOImpl() {
     // TODO Attention, la connexion devra se faire dans une classe externe
-    Config.load("prod.properties");
-    try {
-      Class.forName("org.postgresql.Driver");
-    } catch (ClassNotFoundException e) {
-      System.out.println("Driver postgresql manquant !");
-      System.exit(1);
-    }
-    try {
-      conn = DriverManager.getConnection(Config.getProperty("url"), Config.getProperty("user"),
-          Config.getProperty("password"));
-    } catch (SQLException e) {
-      System.out.println("Impossible de joindre le serveur !");
-      System.exit(1);
-    }
-    try {
-      conn.close();
-      System.out.println("La connexion vient de se fermer");
-    } catch (SQLException e) {
-      System.out.println(e.getMessage());
-    }
+
 
   }
 
