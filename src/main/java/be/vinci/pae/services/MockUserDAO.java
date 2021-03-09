462a25a735483d8fb95bc96cf52975e3a6ee5b96
Kevin Jullien <kevin.jullien@student.vinci.be>
2021-03-09 10:10:34 +0100

    refactor - cleaning

---
+++ src/main/java/be/vinci/pae/services/MockUserDAO.java
@@ -0,0 +1,60 @@
+package be.vinci.pae.services;
+
+import java.time.LocalDateTime;
+import be.vinci.pae.domain.UserDTO;
+import be.vinci.pae.domain.UserFactory;
+import jakarta.inject.Inject;
+
+public class MockUserDAO implements UserDAO {
+
+  @Inject
+  private DalServices dalService;
+
+  @Inject
+  private UserFactory userFactory;
+
+  public MockUserDAO() {
+    // TODO Beware, the connection has to be done in an extern class
+  }
+
+
+  @Override
+  public UserDTO getUser(String email) {
+    UserDTO user = null;
+    if (email == "valid@email.com") {
+        user = userFactory.getUserDTO();
+        user.setId(0);
+        user.setUsername("test");
+        user.setLastName("Jean");
+        user.setFirstName("Cérien");
+        user.setEmail("valid@email.com");
+        user.setRole("client");
+        user.setRegistrationDate(LocalDateTime.now());
+        user.setValidated(true);
+        user.setPassword("$2a$10$9fCguFzUn1ae/wFf.nHFkObDBPQqX8TII5QOaSO/GTNw7iZtLECJu"); //1234
+        user.setAddress(0);
+    }
+    return user;
+  }
+
+
+  @Override
+  public UserDTO getUser(int id) {
+    UserDTO user = null;
+    if (id == 0) {
+        user = userFactory.getUserDTO();
+        user.setId(0);
+        user.setUsername("test");
+        user.setLastName("Jean");
+        user.setFirstName("Cérien");
+        user.setEmail("valid@email.com");
+        user.setRole("client");
+        user.setRegistrationDate(LocalDateTime.now());
+        user.setValidated(true);
+        user.setPassword("$2a$10$9fCguFzUn1ae/wFf.nHFkObDBPQqX8TII5QOaSO/GTNw7iZtLECJu"); //1234
+        user.setAddress(0);
+    }
+    return user;
+  }
+
+}
--- src/main/java/be/vinci/pae/services/UserDAO.java
+++ src/main/java/be/vinci/pae/services/UserDAO.java
@@ -1,6 +1,5 @@
 package be.vinci.pae.services;
 
-import be.vinci.pae.domain.User;
 import be.vinci.pae.domain.UserDTO;
 
 public interface UserDAO {
@@ -8,7 +7,5 @@ public interface UserDAO {
   UserDTO getUser(String email);
 
   UserDTO getUser(int id);
-
-  void addUser(User user);
 
 }
