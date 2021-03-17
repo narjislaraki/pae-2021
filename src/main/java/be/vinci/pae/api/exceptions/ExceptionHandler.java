2650e9899223b362081999e644d9ff1659e0186a
Kevin Jullien <kevin.jullien@student.vinci.be>
2021-03-17 16:59:04 +0100

    feat - stacktrace to string in log file

--- src/main/java/be/vinci/pae/api/exceptions/ExceptionHandler.java
+++ src/main/java/be/vinci/pae/api/exceptions/ExceptionHandler.java
@@ -2,7 +2,9 @@ package be.vinci.pae.api.exceptions;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.logging.Logger;
 
+import be.vinci.pae.utils.APILogger;
 import be.vinci.pae.utils.Config;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Response;
@@ -12,19 +14,23 @@ import jakarta.ws.rs.ext.Provider;
 @Provider
 public class ExceptionHandler implements ExceptionMapper<Throwable> {
 
+  private Logger logger = APILogger.getLogger();
+
 
   @Override
   public Response toResponse(Throwable exception) {
-    exception.printStackTrace();
+    // from stacktrace to String.
+    StringWriter sw = new StringWriter();
+    PrintWriter pw = new PrintWriter(sw);
+    exception.printStackTrace(pw);
+
+    logger.warning(exception.getClass().getCanonicalName() + "\n" + sw.toString());
 
     if (Config.getBoolProperty("SendStackTraceToClient")) {
       return Response.status(getStatusCode(exception)).entity(getEntity(exception)).build();
     }
     return Response.status(getStatusCode(exception))
-        .entity(exception.getMessage() == null || exception.getMessage().isEmpty()
-            ? "Oh no, something wrong happened!"
-            : exception.getMessage())
-        .build();
+        .entity("Oh no, something wrong happened! Don't worry, we're on it!").build();
   }
 
   /*
