59f5d6e0f5808fb1d3f11c7eac6b86ec9809548c
Kevin Jullien <kevin.jullien@student.vinci.be>
2021-03-23 15:56:25 +0100

    merge ok

--- src/test/java/be/vinci/pae/UserUCCTest.java
+++ src/test/java/be/vinci/pae/UserUCCTest.java
@@ -6,15 +6,16 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import be.vinci.pae.api.exceptions.UnauthorizedException;
 import be.vinci.pae.domain.user.User;
 import be.vinci.pae.domain.user.UserUCC;
-import be.vinci.pae.tests.UserDistributor;
+import be.vinci.pae.services.dao.UserDAO;
 import be.vinci.pae.utils.ApplicationBinder;
-import be.vinci.pae.utils.Config;
 
 public class UserUCCTest {
 
@@ -26,16 +27,13 @@ public class UserUCCTest {
   private static String goodPassword;
   private static String badPassword;
   private static String goodEmailNotValidated;
+  private static UserDAO userDAO;
 
   /**
    * Initialisation before every tests.
    */
   @BeforeAll
   public static void init() {
-    Config.load();
-    ServiceLocator locator = ServiceLocatorUtilities.bind(new ApplicationBinder());
-    userUCC = locator.getService(UserUCC.class);
-
     badEmail = "test.test@test.com";
     badPassword = "5678";
 
@@ -45,13 +43,23 @@ public class UserUCCTest {
     goodUserNotValidated = UserDistributor.getGoodNotValidatedUser();
     goodEmailNotValidated = goodUserNotValidated.getEmail();
 
+    ServiceLocator locator =
+        ServiceLocatorUtilities.bind(new ApplicationBinder(), new ApplicationBinderTest());
+    userUCC = locator.getService(UserUCC.class);
+
+    userDAO = locator.getService(UserDAO.class);
+  }
+
+  @BeforeEach
+  public void reset() {
+    Mockito.reset(userDAO);
   }
 
   @DisplayName("Test connection with right email and password")
   @Test
   public void connection1Test() {
-    User u = (User) userUCC.connection(goodEmail, goodPassword);
-    assertEquals(goodUser, u);
+    Mockito.when(userDAO.getUserFromEmail(goodEmail)).thenReturn(goodUser);
+    assertEquals(goodUser, userUCC.connection(goodEmail, goodPassword));
   }
 
   @DisplayName("Test connection with bad email")
@@ -87,6 +95,7 @@ public class UserUCCTest {
   @DisplayName("Test connection with good credentials but the user isn't validated yet")
   @Test
   public void connection7Test() {
+    Mockito.when(userDAO.getUserFromEmail(goodEmailNotValidated)).thenReturn(goodUserNotValidated);
     assertThrows(UnauthorizedException.class,
         () -> userUCC.connection(goodEmailNotValidated, goodPassword));
   }
