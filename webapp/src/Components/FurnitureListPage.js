6be0700d25338e80a145438df956c88d6bdc94a1
Samy Alliche <samyalliche@Samys-MacBook.local>
2021-03-28 15:17:57 +0200

    feat - added new furnitureListPage.js

--- webapp/src/Components/FurnitureListPage.js
+++ webapp/src/Components/FurnitureListPage.js
@@ -11,15 +11,41 @@ let furnitureListTab;
 
 let furnitureListPage = 
 `
+        <div class="all-furn-title small-caps">Tous les meubles</div>
+
+        <div class="parent-furnitures-container">
 
 `;
+
 
 const FurnitureListPage = () => {  
     let page = document.querySelector("#page");
     page.innerHTML = furnitureListPage;
-    
+    let buyableFurnitureList = getBuyableFurnituresList();
+    buyableFurnitureList.forEach(element => {
+        page.innerHTML += 
+        `
+        <div class="item-card">
+            <div class="item-img-container">
+                <img src="${}" alt="" class="item-img">
+                <h3 class="item-img-hover condensed">Voir<br>article</h3>
+            </div>
+            <div class="item-name">${}</div>
+            <div class="item-price condensed">${}</div><div class="currency" style="font-size: 18px;">euro</div>
+        </div>
+    `;
+    });
+
+
+    //close the div
+    page.innerHTML += `</div>`;
     const user = getUserSessionData();
   };
+
+  //return a list of all the buyable furnitures
+  function getBuyableFurnituresList(){
+
+  }
 
 
   export default FurnitureListPage;
