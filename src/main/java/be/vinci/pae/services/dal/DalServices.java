77b49bdf3c674cf070c6f5b6f2081bc68294f714
Kevin Jullien <kevin.jullien@student.vinci.be>
2021-04-13 15:25:53 +0200

    refactor - managed bizzTransaction with autocommit=false in Handler

--- src/main/java/be/vinci/pae/services/dal/DalServices.java
+++ src/main/java/be/vinci/pae/services/dal/DalServices.java
@@ -4,14 +4,37 @@ public interface DalServices {
 
   void getBizzTransaction(boolean autoCommit);
 
+  /**
+   * Commit and close a transaction with auto-commit enabled. A FatalException is thrown if an
+   * SQLException shows up.
+   */
   void commitBizzTransaction();
 
-  void commitTransactionAndContinue();
+  /**
+   * Rollback a transaction with auto-commit enabled. A FatalException is thrown if an SQLException
+   * shows up.
+   */
+  void rollbackBizzTransaction();
 
-  void rollbackTransaction();
-
+  /**
+   * Close a transaction with auto-commit disabled. A FatalException is thrown if an SQLException
+   * shows up.
+   */
   void stopBizzTransaction();
 
-  boolean hasTransaction();
+  /**
+   * Check if the current thread has a transaction.
+   * 
+   * @return true if yes, false if not
+   */
+  boolean hasBizzTransaction();
+
+  /**
+   * Check if the current thread'stransaction is in auto-commit or not. If the current thread has no
+   * transaction, false will be returned.
+   * 
+   * @return true if yes, false if not or no transaction is active
+   */
+  boolean isBizzTransactionInAutoCommit();
 
 }
