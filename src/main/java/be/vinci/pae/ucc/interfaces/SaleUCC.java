232b4b212710cd9e753b7a2a9541207eb117eab5
deTheux_Boris <boris.detheux@student.vinci.be>
2021-05-08 19:12:17 +0200

    add - javadoc for all ucc

--- src/main/java/be/vinci/pae/ucc/interfaces/SaleUCC.java
+++ src/main/java/be/vinci/pae/ucc/interfaces/SaleUCC.java
@@ -6,7 +6,18 @@ import be.vinci.pae.domain.interfaces.SaleDTO;
 
 public interface SaleUCC {
 
+  /**
+   * Get the list of furniture sold.
+   * 
+   * @return the list of sale
+   */
   List<SaleDTO> getSalesList();
 
+  /**
+   * Change the condition of a furniture and add a sale.
+   * 
+   * @param sale the sale to be add
+   * @return true if the sale as been effectuate, false if the furniture is already sold
+   */
   boolean addSale(SaleDTO sale);
 }
--- src/main/java/be/vinci/pae/ucc/interfaces/UserUCC.java
+++ src/main/java/be/vinci/pae/ucc/interfaces/UserUCC.java
@@ -8,21 +8,75 @@ import be.vinci.pae.domain.interfaces.UserDTO;
 
 public interface UserUCC {
 
+  /**
+   * Connect a user if the credential are correct and if the user is validated.
+   * 
+   * @param email the email introduce
+   * @param password the password introduce
+   * @return the user to be connected
+   */
   UserDTO connection(String email, String password);
 
+  /**
+   * Register a user with the information entered.
+   * 
+   * @param user the user to be create
+   * @return true if the user is create, otherwise false
+   */
   boolean registration(UserDTO user);
 
+  /**
+   * Validated a user and set his role.
+   * 
+   * @param id the id of the user
+   * @param role the role to be assign
+   * @return true if the user has been accepted, otherwise false
+   */
   boolean acceptUser(int id, String role);
 
+  /**
+   * Delete the user associate to the id.
+   * 
+   * @param id of the user
+   * @return true if the user has been deleted, otherwise false
+   */
   boolean deleteUser(int id);
 
+  /**
+   * Get the list of the unvalidated users.
+   * 
+   * @return the list of the unvalitated users
+   */
   List<UserDTO> getUnvalidatedUsers();
 
+  /**
+   * Get a user by his id.
+   * 
+   * @param id the id of the user
+   * @return the user associate to the id, otherwise throw an exeception
+   */
   UserDTO getUserFromId(int id);
 
+  /**
+   * Get the list of validated users.
+   * 
+   * @return the list of validated users
+   */
   List<UserDTO> getValidatedUsers();
 
+  /**
+   * Get a list of sale by his buyer id.
+   * 
+   * @param id the id of the user
+   * @return the list of sale
+   */
   List<SaleDTO> getTransactionsBuyer(int id);
 
+  /**
+   * Get by and id user, the list of furniture sold by this user.
+   * 
+   * @param id the id of the user.
+   * @return the list of furniture sold
+   */
   List<FurnitureDTO> getTransactionsSeller(int id);
 }
